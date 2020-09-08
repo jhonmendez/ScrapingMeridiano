7fee1d4eca11f87f8c331f525ceff5801c16fa74
jhonjaimemendezalandete@gmail.com <heroku buildpacks:clear>
2020-09-08 15:35:16 -0500

    Se agregan las pruebas automaticas

--- edu.cecar.scraping/src/main/java/edu/cecar/scraping/Scraper.java
+++ edu.cecar.scraping/src/main/java/edu/cecar/scraping/Scraper.java
@@ -12,14 +12,14 @@
 
 package edu.cecar.scraping;
 
-import java.io.IOException;
-
-
+import java.sql.PreparedStatement;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+
+import edu.cecar.componentes.basesdedatos.ConectarMySQL;
 
 /**
  *  Clase que scrapea una pagina del sitio web del meridiano
@@ -27,42 +27,60 @@ import org.jsoup.select.Elements;
  */
 
 public class Scraper {
-	
-	private void scrapingPaginaMeridiano(String url) {
-		
+
+
+
+	public void scrapingPaginaMeridiano(String url) {
+
 		try {
-			
+
 			Document document = Jsoup.connect(url).get();
-			
-			
-			
+
 			Elements div = document.select("div.News-content-description");
 			Element element = div.get(0);
-			
+
 			String titulo = document.select("h1").text();
 			String contenido = element.text();
+
+			String insertarArticulo = "Insert into articulos values(null,?,?,sysdate())";
 			
-			System.out.println("Titulo: " +  titulo);
-			System.out.println("Contenido: " + contenido);
+			PreparedStatement preparedStatement = ConectarMySQL.getConexion().
+					                               prepareStatement(insertarArticulo);
+			preparedStatement.setString(1, titulo);
+			preparedStatement.setString(2, contenido);
+			preparedStatement.execute();
 			
-			
-		} catch (IOException e) {
+			System.out.println("Articulo insertado de manera exitosa");
+
+
+		} catch (Exception e) {
 			e.printStackTrace();
-			
-		}
-		
-		
+
+		} 
+
+
 	}
-	
-	
+
+
 	public static void main(String[] args) {
-		
-		String url = "https://elmeridiano.co/noticia/confirman-crimen-de-policia-en-el-bajo-cauca-antioque%C3%B1o-";
-		
-		new Scraper().scrapingPaginaMeridiano(url);
-		
+
+		//Se realiza la conexion a bases de datos
+		try {
+			
+			ConectarMySQL.getConectarMySQL("127.0.0.1", "analisis", "root", "");
+
+			String url = "https://elmeridiano.co/noticia/confirman-crimen-de-policia-en-el-bajo-cauca-antioque%C3%B1o-";
+
+			new Scraper().scrapingPaginaMeridiano(url);
+
+		} catch (Exception e) {
+			
+			e.printStackTrace();
+
+		}
+
 	}
-	
-	
+
+
 
 }
--- edu.cecar.scraping/src/test/java/edu/cecar/scraping/AppTest.java
+++ edu.cecar.scraping/src/test/java/edu/cecar/scraping/AppTest.java
@@ -1,20 +1,81 @@
+/**
+ * @Clase: AppTest.java
+ * 
+ * @version  1.0
+ * 
+ * @since 8/09/2020
+ * 
+ * @autor Ing. Jhon Mendez
+ *
+ * @Copyrigth: Legal Credit Solution
+ */
+
 package edu.cecar.scraping;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+import edu.cecar.componentes.basesdedatos.ConectarMySQL;
 
 /**
- * Unit test for simple App.
+ *  Descripción de la clase
+ *  
  */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
-    }
+
+public class AppTest {
+	
+	@Test
+	public void scrapingPaginaMeridianoTest() {
+		
+		//Se define los datos e informacion esperada
+		String tituloEsperado = "Confirman crimen de policía en el Bajo Cauca antioqueño";
+		String contenidoEsperado = "Jaime Luis Castaño Castillo, fue el miembro de la Policía asesinado en el sector de la Gloria en la vía que conduce de Caucasia a El Bagre.\r\n" + 
+				"\r\n" + 
+				"El uniformado murió al trasladarlo al hospital César Uribe Piedrahita de Caucasia. Inicialmente se hablaba de dos policías muertos, pero se confirmó que fue uno y dos más están gravemente heridos.\r\n" + 
+				"\r\n" + 
+				"El vehículo en el que se movilizaban los uniformados quedó casi destruido al ser atacado a bala por miembros de una de las bandas criminales que operan en esa región.";
+		
+		
+		contenidoEsperado = contenidoEsperado.replaceAll("\r|\n", " ").replaceAll("( )+", " ");
+		
+		//Se obtiene los datos e información resultante del software
+		try {
+			
+			
+			ConectarMySQL.getConectarMySQL("127.0.0.1", "analisis", "root", "");
+			String url = "https://elmeridiano.co/noticia/confirman-crimen-de-policia-en-el-bajo-cauca-antioque%C3%B1o-";
+			Scraper scraper = new Scraper();
+			scraper.scrapingPaginaMeridiano(url);
+			
+			PreparedStatement consultarArticulo = ConectarMySQL.getConexion().prepareStatement("select titulo, contenido " + 
+																								"from articulos " + 
+																								"order by identificador desc " + 
+																								"limit 1");
+			ResultSet resultado = consultarArticulo.executeQuery();
+			resultado.next();
+			
+			String tituloObtenido = resultado.getString(1);
+			String contenidoObtenido = resultado.getString(2);
+			
+			assertEquals("El titulo del articulo no corresponde",tituloEsperado,tituloObtenido);
+			assertEquals("El contenido del articulo no corresponde",contenidoEsperado,contenidoObtenido);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail("Error de conexion de la bases de datos " + e);
+			
+		}
+
+		
+
+		
+		
+	}
+
 }
